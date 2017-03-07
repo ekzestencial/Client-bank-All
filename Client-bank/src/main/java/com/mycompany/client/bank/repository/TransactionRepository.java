3d7874970acc0e9653a6577a22d37058893b853b
artem7902 <artem7902@yandex.ru>
2017-03-07 12:56:58 -0800

    Add many methods to my repository

--- Client-bank/src/main/java/com/mycompany/client/bank/repository/TransactionRepository.java
+++ Client-bank/src/main/java/com/mycompany/client/bank/repository/TransactionRepository.java
@@ -5,15 +5,29 @@
  */
 package com.mycompany.client.bank.repository;
 
+import com.mycompany.client.bank.jpa.Account;
 import com.mycompany.client.bank.jpa.Transaction;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
-
+import java.util.Date;
 /**
  *
  * @author artem
  */
 public interface TransactionRepository extends CrudRepository<Transaction, Long>{
-    @Override
+             @Override
      public List<Transaction> findAll();
+    public Transaction findById(Long id);
+    public List<Transaction> findByValue(int value);
+    public List<Transaction> findByValueLessThanOrEquality(int value);
+    public List<Transaction> findByValueGreaterThanOrEquality(int value);
+    public List<Transaction> findByDate(Date date);
+    public List<Transaction> findByDateLessThanOrEquality(Date date);
+    public List<Transaction> findByDateGreaterThanOrEquality(Date date);
+    public List<Transaction> findByAccaountId(Account accaountId);
+    public Transaction removeById(Long id);
+    public List<Transaction> removeByValueLessThanOrEquality(int value);
+    public List<Transaction> removeByValueGreaterThanOrEquality(int value);
+    public List<Transaction> removeByDate(Date date);
+    public List<Transaction> removeByAccaountId(Account accaountId);
 }
