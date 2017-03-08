046e5db8081a0d4051674744cc7edf24113cd894
Alexander <ekzestencial@gmail.com>
2017-03-08 23:00:32 +0200

    Added the first test

---
+++ Client-bank/src/test/java/com/mycompany/client/bank/services/User/GetAllUsersTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.client.bank.services.User;
+
+import com.mycompany.client.bank.services.UserService;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author ekzestencial
+ */
+public class GetAllUsersTest {
+
+	@Autowired
+	UserService user;
+
+	public GetAllUsersTest() {
+	}
+
+	@BeforeClass
+	public static void setUpClass() {
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+	}
+
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	@Test
+	public void findAllUsers() {
+		//int count = user.getAllUsers().size();
+		//assertEquals(4, count);
+
+	}
+}
