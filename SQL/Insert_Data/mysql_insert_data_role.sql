eb9347c19827f24a40da8c1894df0f32dac5b375
Alexander <ekzestencial@gmail.com>
2017-03-08 21:44:00 +0200

    modified roles.sql

--- SQL/Insert_Data/mysql_insert_data_role.sql
+++ SQL/Insert_Data/mysql_insert_data_role.sql
@@ -1,4 +1,6 @@
 
-INSERT INTO appuser(user_id, username, password, email, reg_date,last_activity,role_id) 
-            VALUES (1, 'client1','d8578edf8458ce06fbc5bb76a58c5','client1@test.com','1998-12-31','2016-12-31','0');
-INSERT INTO 
+INSERT INTO role(role_id,name) 
+            VALUES ('0','admin');
+
+INSERT INTO role(role_id,name) 
+            VALUES ('1','user');
