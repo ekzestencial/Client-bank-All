cadc0af88ea787210e01e285b0de540d7b80d2c9
Alexander <ekzestencial@gmail.com>
2017-03-16 01:56:52 +0200

    The column "wallet" into the table "appuser" was added;

--- Client-bank/src/main/java/com/mycompany/client/bank/jpa/Appuser.java
+++ Client-bank/src/main/java/com/mycompany/client/bank/jpa/Appuser.java
@@ -6,10 +6,8 @@
 package com.mycompany.client.bank.jpa;
 
 import java.io.Serializable;
-import java.util.Collection;
 import java.util.Date;
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -17,20 +15,19 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
  * @author ekzestencial
  */
 @Entity
-@Table(name = "appuser", schema = "")
+@Table(name = "appuser")
 @XmlRootElement
 @NamedQueries({
 	@NamedQuery(name = "Appuser.findAll", query = "SELECT a FROM Appuser a")
@@ -39,40 +36,49 @@ import javax.xml.bind.annotation.XmlTransient;
 	, @NamedQuery(name = "Appuser.findByPassword", query = "SELECT a FROM Appuser a WHERE a.password = :password")
 	, @NamedQuery(name = "Appuser.findByEmail", query = "SELECT a FROM Appuser a WHERE a.email = :email")
 	, @NamedQuery(name = "Appuser.findByRegDate", query = "SELECT a FROM Appuser a WHERE a.regDate = :regDate")
-	, @NamedQuery(name = "Appuser.findByLastActivity", query = "SELECT a FROM Appuser a WHERE a.lastActivity = :lastActivity")})
+	, @NamedQuery(name = "Appuser.findByLastActivity", query = "SELECT a FROM Appuser a WHERE a.lastActivity = :lastActivity")
+	, @NamedQuery(name = "Appuser.findByWallet", query = "SELECT a FROM Appuser a WHERE a.wallet = :wallet")})
 public class Appuser implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	@Id
         @Basic(optional = false)
+        @NotNull
         @Column(name = "user_id")
 	private Long userId;
 	@Basic(optional = false)
+        @NotNull
+        @Size(min = 1, max = 20)
         @Column(name = "username")
 	private String username;
 	@Basic(optional = false)
+        @NotNull
+        @Size(min = 1, max = 40)
         @Column(name = "password")
 	private String password;
+	// @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
 	@Basic(optional = false)
+        @NotNull
+        @Size(min = 1, max = 40)
         @Column(name = "email")
 	private String email;
 	@Basic(optional = false)
+        @NotNull
         @Column(name = "reg_date")
         @Temporal(TemporalType.DATE)
 	private Date regDate;
 	@Basic(optional = false)
+        @NotNull
         @Column(name = "last_activity")
         @Temporal(TemporalType.TIMESTAMP)
 	private Date lastActivity;
+	@Basic(optional = false)
+        @NotNull
+        @Column(name = "wallet")
+	private double wallet;
 	@JoinColumn(name = "role_id", referencedColumnName = "role_id")
         @ManyToOne(optional = false)
 	private Role roleId;
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "userId")
-	private Collection<Notification> notificationCollection;
-	@OneToOne(cascade = CascadeType.ALL, mappedBy = "appuser")
-	private Userdetails userdetails;
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "userId")
-	private Collection<Account> accountCollection;
 
 	public Appuser() {
 	}
@@ -81,13 +87,14 @@ public class Appuser implements Serializable {
 		this.userId = userId;
 	}
 
-	public Appuser(Long userId, String username, String password, String email, Date regDate, Date lastActivity) {
+	public Appuser(Long userId, String username, String password, String email, Date regDate, Date lastActivity, double wallet) {
 		this.userId = userId;
 		this.username = username;
 		this.password = password;
 		this.email = email;
 		this.regDate = regDate;
 		this.lastActivity = lastActivity;
+		this.wallet = wallet;
 	}
 
 	public Long getUserId() {
@@ -138,38 +145,20 @@ public class Appuser implements Serializable {
 		this.lastActivity = lastActivity;
 	}
 
+	public double getWallet() {
+		return wallet;
+	}
+
+	public void setWallet(double wallet) {
+		this.wallet = wallet;
+	}
+
 	public Role getRoleId() {
 		return roleId;
 	}
 
 	public void setRoleId(Role roleId) {
 		this.roleId = roleId;
-	}
-
-	@XmlTransient
-	public Collection<Notification> getNotificationCollection() {
-		return notificationCollection;
-	}
-
-	public void setNotificationCollection(Collection<Notification> notificationCollection) {
-		this.notificationCollection = notificationCollection;
-	}
-
-	public Userdetails getUserdetails() {
-		return userdetails;
-	}
-
-	public void setUserdetails(Userdetails userdetails) {
-		this.userdetails = userdetails;
-	}
-
-	@XmlTransient
-	public Collection<Account> getAccountCollection() {
-		return accountCollection;
-	}
-
-	public void setAccountCollection(Collection<Account> accountCollection) {
-		this.accountCollection = accountCollection;
 	}
 
 	@Override
