3896d36f617ee43f75579d68f15faf5f46b2a913
Slava Huneiko <guneikoslava@gmail.com>
2017-03-19 18:35:46 +0200

    added LibAccount LibNotif and AccountMapper

---
+++ Client-bank-API/src/main/java/com/mycompany/client/bank/api/LibAccount.java
@@ -0,0 +1,27 @@
+package com.mycompany.client.bank.api;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class LibAccount {
+	
+	@XmlElement(required = false)
+	public Long accountId;
+	@XmlElement(required = true)
+	public double value;
+	@XmlElement(required = true)
+	public String openDate;
+	@XmlElement(required = true)
+	public Long userId;
+	@XmlElement(required = true)
+	public Long bankId;
+	
+	@Override
+	public int hashCode() {
+		int result = 17;
+		result = 37 * result + userId.hashCode();
+		result = 37 * result + bankId.hashCode();
+		return result;
+	}
+}
